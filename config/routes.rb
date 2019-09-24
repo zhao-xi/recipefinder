9378d43ae78f985e24e162ceb8f032eda7109d14
Ubuntu <ubuntu@ip-172-31-35-243.ap-northeast-1.compute.internal>
2019-09-24 07:08:07 +0000

    change root to /recipes_controller/index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'recipes_controller/index'
 
+  root 'recipes_controller#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
