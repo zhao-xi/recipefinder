e166254cf8cfe16c97fe8d70a699578b89b0a237
Zhao Xi <zhaoxi0418@outlook.com>
2019-10-15 00:16:04 +0900

    update api key

--- app/controllers/recipes_controller_controller.rb
+++ app/controllers/recipes_controller_controller.rb
@@ -1,6 +1,6 @@
 class RecipesControllerController < ApplicationController
   def index
-    @keyword = params[:search]
+    @keyword = params[:search] || "chocolate"
     @recipes = Recipe.for @keyword
   end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,7 +4,7 @@ class Recipe < ActiveRecord::Base
 	default_options.update(verify: false) # Turn off SSL verification
 	hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'www.food2fork.com'
 	base_uri 'http://food2fork.com/api'
-	ENV["FOOD2FORK_KEY"] = 'd1998834a6670834265bcac40a30ec34'
+	ENV["FOOD2FORK_KEY"] = '0650f8a89cde23e806d5d04d222e58d4'
 	default_params key: ENV["FOOD2FORK_KEY"]
 	format :json
 
